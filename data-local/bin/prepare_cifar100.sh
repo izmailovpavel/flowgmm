3817ffb8c60b83124a050fc0e395221ab95557e4
Pavel Izmailov <izmailovpavel@gmail.com>
2019-04-02 14:18:50 -0400

    added ssl data prerparation scripts

---
+++ data-local/bin/prepare_cifar100.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "Downloading and unpacking CIFAR-100"
+mkdir -p $DIR/../workdir
+python3 $DIR/unpack_cifar100.py $DIR/../workdir $DIR/../images/cifar/cifar100/by-image/
+
+echo "Linking training set"
+(
+    cd $DIR/../images/cifar/cifar100/by-image/
+    bash $DIR/link_cifar100_train.sh
+)
+
+echo "Linking validation set"
+(
+    cd $DIR/../images/cifar/cifar100/by-image/
+    bash $DIR/link_cifar100_val.sh
+)
