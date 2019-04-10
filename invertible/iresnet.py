55eaf392938b836cadabe09b119a8fcce19c9611
Marc Finzi <mfinzi@hmc.edu>
2019-04-10 18:44:00 -0400

    removed unintentionally added log files from the repo

--- invertible/iresnet.py
+++ invertible/iresnet.py
@@ -10,34 +10,38 @@ from oil.architectures.parts import conv2d,ResBlock
 from blocks import ConcatResBlock,ODEBlock,RNNBlock
 from blocks import ConcatBottleBlock,BezierResBlock
 from downsample import SqueezeLayer,split,merge,padChannels,keepChannels
-from clipped_BN import clippedBN
-from torch.nn.utils import spectral_norm
-import auto_inverse
+from clipped_BN import MeanOnlyBN, iBN
+#from torch.nn.utils import spectral_norm
+from auto_inverse import iSequential
+from spectral_norm import SN
 
-def add_spectral_norm(module):
-    if isinstance(module,  (nn.ConvTranspose1d,
-                            nn.ConvTranspose2d,
-                            nn.ConvTranspose3d,
-                            nn.Conv1d,
-                            nn.Conv2d,
-                            nn.Conv3d)):
-        spectral_norm(module,dim = 1)
-        #print("SN on conv layer: ",module)
-    elif isinstance(module, nn.Linear):
-        spectral_norm(module,dim = 0)
+
+# def add_spectral_norm(module):
+#     if isinstance(module,  (nn.ConvTranspose1d,
+#                             nn.ConvTranspose2d,
+#                             nn.ConvTranspose3d,
+#                             nn.Conv1d,
+#                             nn.Conv2d,
+#                             nn.Conv3d)):
+#         spectral_norm(module,dim = 1)
+#         #print("SN on conv layer: ",module)
+#     elif isinstance(module, nn.Linear):
+#         spectral_norm(module,dim = 0)
 
 class iResBlock(nn.Module):
     def __init__(self,in_channels,out_channels,ksize=3,drop_rate=0,stride=1,
-                    gn=False,inverse_tol=1e-2,**kwargs):
+                    inverse_tol=1e-5,sigma=1.,**kwargs):
         super().__init__()
+        assert stride==1, "Only stride 1 supported"
         #norm_layer = (lambda c: nn.GroupNorm(c//16,c)) if gn else nn.BatchNorm2d
         self.net = nn.Sequential(
-            clippedBN(in_channels),
+            MeanOnlyBN(in_channels),
             nn.ReLU(),
-            conv2d(in_channels,out_channels,ksize,**kwargs),
-            clippedBN(out_channels),
+            SN(conv2d(in_channels,out_channels,ksize,**kwargs)),
+            MeanOnlyBN(out_channels),
             nn.ReLU(),
-            conv2d(out_channels,out_channels,ksize,stride=stride,**kwargs),
+            SN(conv2d(out_channels,out_channels,ksize,**kwargs)),
+            Expression(lambda x: sigma*x),
             nn.Dropout(p=drop_rate)
         )
         assert in_channels == out_channels
@@ -46,7 +50,8 @@ class iResBlock(nn.Module):
         self.inverse_tol = inverse_tol
         self.reverse_iters = 0
         self.inverses_evaluated = 0
-        self.apply(add_spectral_norm)
+        self.x_y = None
+        #self.apply(add_spectral_norm)
 
     @property
     def iters_per_reverse(self):
@@ -55,6 +60,7 @@ class iResBlock(nn.Module):
     def forward(self,inp):
         x,z = inp
         y = x + self.net(x)
+        self.x_y = x,y
         return y,z # autograd will not traverse z_out?
 
     def inverse(self,output):
@@ -62,27 +68,37 @@ class iResBlock(nn.Module):
         self.inverses_evaluated +=1
         with torch.no_grad():
             x_prev = y
+            x_next = y
             diff = 1
-            while diff < self.inverse_tol:
+            for i in range(100):#while diff > self.inverse_tol:
                 x_next = y - self.net(x_prev)
-                diff = torch.norm(x_next-x_prev)/(torch.norm(x_prev)+1e-8)
+                diff = torch.norm(x_next-x_prev)/(torch.max(torch.norm(y),torch.tensor(1e-15).to(y.device)))
                 x_prev = x_next
                 self.reverse_iters +=1
+                if diff<self.inverse_tol: break
+            #print(diff,self.reverse_iters)
         return x_prev,z
 
-    def logdet(self,inp):
-        raise NotImplementedError
+    def logdet(self):
+        assert self.x_y is not None, "logdet called before forwards"
+        x,y = self.x_y
+        lndet = 0
+        w = v = torch.randn_like(y)
+        for k in range(1,2):
+            w = jvp(x,y,w)
+            #print(len(w))
+            wTv = (w*v).sum(3).sum(2).sum(1) # dont sum over batch dim
+            #print(f"iResblock mul {k}: {wTv}")
+            lndet += ((-1)**(k+1))*wTv/k
+        #print(w.shape)
+        #print(f"iResblock logdet {lndet}")
+        return lndet
+            
 
-@export
-def conv2d(in_channels,out_channels,kernel_size=3,coords=False,dilation=1,**kwargs):
-    """ Wraps nn.Conv2d and CoordConv, padding is set to same
-        and coords=True can be specified to get additional coordinate in_channels"""
-    assert 'padding' not in kwargs, "assumed to be padding = same "
-    same = (kernel_size//2)*dilation
-    if coords: 
-        return CoordConv(in_channels,out_channels,kernel_size,padding=same,dilation=dilation,**kwargs)
-    else: 
-        return nn.Conv2d(in_channels,out_channels,kernel_size,padding=same,dilation=dilation,**kwargs)
+def jvp(x,y,v,retain_graph=True):
+    with torch.autograd.enable_grad():
+        vJ = torch.autograd.grad(y,x,v,create_graph=retain_graph,retain_graph=retain_graph)[0]
+    return vJ
 
 class iConv2d(nn.Module):
     """ wraps conv2d in a module with an inverse function """
@@ -97,7 +113,7 @@ class iConv2d(nn.Module):
         return self.conv(inp)
     def inverse(self,output):
         pass
-    def logdet(self,inp):
+    def logdet(self):
         raise NotImplementedError
 
 class addZslot(nn.Module):
@@ -109,7 +125,21 @@ class addZslot(nn.Module):
         x,z = output
         assert not z, "nonempty z received"
         return x
-    def logdet(self,inp):
+    def logdet(self):
+        return 0
+
+class Join(nn.Module):
+    def __init__(self):
+        super().__init__()
+    def forward(self,x):
+        y,z = x
+        z.append(y)
+        return z
+    def inverse(self,z):
+        #y = z.pop()
+        z,y = z[:-1],z[-1]
+        return y,z
+    def logdet(self):
         return 0
 
 def BNrelu(channels,gn=False):
@@ -118,7 +148,7 @@ def BNrelu(channels,gn=False):
 
 def BN(channels,gn=False):
     return nn.GroupNorm(channels//16,channels) if gn else nn.BatchNorm2d(channels)
-    
+
 
 @export
 class aResnet(nn.Module,metaclass=Named):
@@ -140,71 +170,184 @@ class aResnet(nn.Module,metaclass=Named):
     def forward(self,x):
         return self.net(x)
 
-# @export
-# class squeezeResnet(nn.Module,metaclass=Named):
-#     def __init__(self,num_classes=10,k=64,gn=False):
-#         super().__init__()
-#         self.num_classes = num_classes
-#         self.net = nn.Sequential(
-#             conv2d(3,k),
-#             ResBlock(k,k,gn=gn),
-#             SqueezeLayer(2),
-#             ResBlock(4*k,2*k,gn=gn),
-#             ResBlock(2*k,2*k,gn=gn),
-#             SqueezeLayer(2),
-#             ResBlock(8*k,4*k,gn=gn),
-#             ResBlock(4*k,4*k,gn=gn),
-#             SqueezeLayer(2),
-#             ResBlock(16*k,8*k,gn=gn),
-#             ResBlock(8*k,8*k,gn=gn),
-#             BNrelu(8*k,gn=gn),
-#             Expression(lambda u:u.mean(-1).mean(-1)),
-#             nn.Linear(8*k,num_classes)
-#         )
-#     def forward(self,x):
-#         return self.net(x)
+class Id(nn.Module):
+    def __init__(self):
+        super().__init__()
+    def forward(self,x):
+        return x
+    def inverse(self,y):
+        return y
+    def logdet(self):
+        return 0
 
-# Id = nn.Sequential
+I = Id()
 
-# class both(nn.Module):
-#     def __init__(self,module1,module2):
-#         super().__init__()
-#         self.module1 = module1
-#         self.module2 = module2
-#     def forward(self,x,z):
-#         return self.module1(x),self.module2(z)
-#     def inverse(self,y,z_out):
-#         return self.module1.inverse(y),self.module2.inverse(z_out)
+class both(nn.Module):
+    def __init__(self,module1,module2):
+        super().__init__()
+        self.module1 = module1
+        self.module2 = module2
+    def forward(self,inp):
+        x,z = inp
+        return self.module1(x),self.module2(z)
+    def inverse(self,output):
+        y,z_out = output
+        return self.module1.inverse(y),self.module2.inverse(z_out)
+    def logdet(self):
+        return self.module1.logdet() + self.module2.logdet()
+
+# Converts a list of torch.tensors into a single flat torch.tensor
+def flatten(tensorList):
+    flatList = []
+    for t in tensorList:
+        flatList.append(t.contiguous().view(t.numel()))
+    return torch.cat(flatList)
+
+# Takes a flat torch.tensor and unflattens it to a list of torch.tensors
+#    shaped like likeTensorList
+def unflatten_like(vector, likeTensorList):
+    outList = []
+    i=0
+    for tensor in likeTensorList:
+        n = tensor.numel()
+        outList.append(vector[i:i+n].view(tensor.shape))
+        i+=n
+    return outList
 
 @export
 class iResnet(nn.Module,metaclass=Named):
-    def __init__(self,num_classes=10,k=64,gn=False):
+    def __init__(self,num_classes=10,k=32,sigma=1.):
         super().__init__()
         self.num_classes = num_classes
-        self.foot = nn.Sequential(
+        self.body = iSequential(
             padChannels(k-3),
-            conv2d(k,k),
             addZslot(),
-        )
-        self.body = nn.Sequential(
-            iResBlock(k,k,gn=gn),
-            SqueezeLayer(2),
-            iResBlock(4*k,4*k,gn=gn),
+            #both(iBN(k),I),
+            iResBlock(k,k,sigma=sigma),
+            both(SqueezeLayer(2),I),
+            #both(iBN(4*k),I),
+            iResBlock(4*k,4*k,sigma=sigma),
             keepChannels(2*k),
-            iResBlock(2*k,2*k,gn=gn),
-            SqueezeLayer(2),
-            iResBlock(8*k,8*k,gn=gn),
+            #both(iBN(2*k),I),
+            iResBlock(2*k,2*k,sigma=sigma),
+            both(SqueezeLayer(2),I),
+            #both(iBN(8*k),I),
+            iResBlock(8*k,8*k,sigma=sigma),
             keepChannels(4*k),
-            iResBlock(4*k,4*k,gn=gn),
-            SqueezeLayer(2),
-            iResBlock(16*k,16*k,gn=gn),
+            #both(iBN(4*k),I),
+            iResBlock(4*k,4*k,sigma=sigma),
+            both(SqueezeLayer(2),I),
+            #both(iBN(16*k),I),
+            iResBlock(16*k,16*k,sigma=sigma),
             keepChannels(8*k),
-            iResBlock(8*k,8*k,gn=gn),
+            #both(iBN(8*k),I),
+            iResBlock(8*k,8*k,sigma=sigma),
+            Join(),
         )
         self.head = nn.Sequential(
-            BNrelu(8*k,gn=gn),
+            BNrelu(8*k),
             Expression(lambda u:u.mean(-1).mean(-1)),
             nn.Linear(8*k,num_classes)
+        )
+        self.k = k
+    @property
+    def z_shapes(self):
+        # For CIFAR10: starting size = 32x32
+        h = w = 32
+        channels = self.k
+        shapes = []
+        for module in self.body:
+            if isinstance(module,keepChannels):
+                #print(module)
+                channels = 2*channels
+                h //=2
+                w //=2
+                shapes.append((channels,h,w))
+        shapes.append((channels,h,w))
+        return shapes
+
+    @property
+    def device(self):
+        try: return self._device
+        except AttributeError:
+            self._device = next(self.parameters()).device
+            return self._device
+
+    def forward(self,x):
+        return self.head(self.body(x)[-1])
+    
+    def get_all_z_squashed(self,x):
+        return flatten(self.body(x))
+
+    def logdet(self):
+        return self.body.logdet()
+    
+    def inverse(self,z):
+        return self.body.inverse(z)
+
+    def sample(self,bs=1):
+        z_all = [torch.randn(bs,*shape).to(self.device) for shape in self.z_shapes]
+        return self.inverse(z_all)
+
+@export
+class iResnetLarge(iResnet):
+    def __init__(self,num_classes=10,k=32,sigma=1.,block_size=4):
+        super().__init__()
+        self.num_classes = num_classes
+        self.body = iSequential(
+            padChannels(k-3),
+            addZslot(),
+            both(iBN(k),I),
+            *[iResBlock(k,k,sigma=sigma) for _ in range(block_size)],
+            both(SqueezeLayer(2),I),
+            both(iBN(4*k),I),
+            *[iResBlock(4*k,4*k,sigma=sigma) for _ in range(block_size)],
+            both(SqueezeLayer(2),I),
+            both(iBN(16*k),I),
+            iResBlock(16*k,16*k,sigma=sigma),
+            keepChannels(8*k),
+            both(iBN(8*k),I),
+            *[iResBlock(8*k,8*k,sigma=sigma) for _ in range(block_size)],
+        )
+        self.head = nn.Sequential(
+            BNrelu(8*k),
+            Expression(lambda u:u.mean(-1).mean(-1)),
+            nn.Linear(8*k,num_classes)
+        )
+@export
+class iResnetLargeV2(nn.Module,metaclass=Named):
+    def __init__(self,num_classes=10,k=64,sigma=1.,block_size=4):
+        super().__init__()
+        self.num_classes = num_classes
+        self.foot = iSequential(
+            padChannels(k-3),
+            #iConv2d(k,k),
+            addZslot(),
+        )
+        self.body = iSequential(
+            both(iBN(k),I),
+            *[iResBlock(k,k,sigma=sigma) for _ in range(block_size)],
+            keepChannels(k//2),
+            both(SqueezeLayer(2),I),
+            both(iBN(2*k),I),
+            *[iResBlock(2*k,2*k,sigma=sigma) for _ in range(block_size)],
+            keepChannels(k),
+            both(SqueezeLayer(2),I),
+            both(iBN(4*k),I),
+            *[iResBlock(4*k,4*k,sigma=sigma) for _ in range(block_size)],
+            keepChannels(2*k),
+            both(SqueezeLayer(2),I),
+            both(iBN(8*k),I),
+            *[iResBlock(8*k,8*k,sigma=sigma) for _ in range(block_size)],
+            keepChannels(4*k),
+            both(SqueezeLayer(2),I),
+            both(iBN(16*k),I),
+            *[iResBlock(16*k,16*k,sigma=sigma) for _ in range(block_size)],
+        )
+        self.head = nn.Sequential(
+            BNrelu(16*k),
+            Expression(lambda u:u.mean(-1).mean(-1)),
+            nn.Linear(16*k,num_classes)
         )
     def forward(self,x):
         y,z = self.body(self.foot(x))
--- invertible/ode_study.py
+++ invertible/ode_study.py
@@ -4,21 +4,22 @@ from oil.model_trainers.classifier import Classifier,simpleClassifierTrial
 # from resnets import SplitODEResnet,ODEResnet,LongResnet,RNNBottle
 # from resnets import SmallResnet,RNNResnet
 # from resnets import BezierRNN,BezierODE,BezierRNNSplit
-from iresnet import iResnet
+from iresnet import iResnet,iResnetLarge,iResnetLargeV2
 from oil.tuning.study import Study, train_trial
 # from oil.tuning.configGenerator import uniform,logUniform
 
-log_dir_base = os.path.expanduser('~/tb-experiments/iresnet_singular2')
+log_dir_base = os.path.expanduser('~/tb-experiments/iresnet_inv_test_sfixed')
 cfg_spec = {
-    'dataset': CIFAR10,
+    'dataset': [CIFAR10],
     'network': iResnet,
-    'net_config': {'sigma':[.7,.8]},
+    'net_config': {'sigma':[.5],'k':32},
     'loader_config': {'amnt_dev':5000,'lab_BS':64},
     'opt_config':{'lr':.1},
-    'num_epochs':[25,50], 
+    'num_epochs':10, 
     'trainer_config':{'log_dir':lambda cfg:log_dir_base+\
         '/{}/{}/s{}'.format(cfg['dataset'],cfg['network'],cfg['net_config']['sigma'])}
     }
+#'log_dir':lambda cfg:f'{log_dir_base}/{cfg['dataset']}/{cfg['network']}/s{cfg['net_config']['sigma']}'
 #ODEResnet,RNNResnet,,SplitODEResnet,SmallResnet,BezierRNNSplit,BezierODE,BezierRNN
 do_trial = simpleClassifierTrial(strict=True)
 ode_study = Study(do_trial,cfg_spec)
