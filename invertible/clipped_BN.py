added sv computation, corrected SN, now bias is correctly subtracted from the conv

invertible/clipped_BN.py
@@ -53,7 +53,7 @@ class MeanOnlyBN(nn.BatchNorm2d):
         )
         bias_var = sumvar / numel
         if self.track_running_stats and not self.training:
-            mean, bias_var, = self.running_mean,self.running_var
+            mean, bias_var = self.running_mean,self.running_var
         inv_std = 1 / (bias_var + self.eps).pow(0.5).unsqueeze(1)
         mul = torch.min(inv_std * self.weight.unsqueeze(1),torch.ones_like(inv_std))
         output = ((input_ - mean.unsqueeze(1))  + self.bias.unsqueeze(1))
@@ -102,16 +102,21 @@ class iBN(nn.BatchNorm2d):
         )
         bias_var = sumvar / numel
         if self.track_running_stats and not self.training:
-            mean, bias_var, = self.running_mean,self.running_var
+            mean, bias_var = self.running_mean,self.running_var
         inv_std = 1 / (bias_var + self.eps).pow(0.5).unsqueeze(1)
+        self.numel,self.height,self.width,self.batchsize = numel,height,width,batchsize
         mul = inv_std * self.weight.unsqueeze(1)
+        self.inv_std = inv_std.detach()
         output = ((input_ - mean.unsqueeze(1))*mul  + self.bias.unsqueeze(1))
 
         return output.view(channels, batchsize, height, width).permute(1, 0, 2, 3).contiguous()
 
-    def logdet(self,input):
-        raise NotImplementedError
-        
+    def logdet(self):
+        mul = self.inv_std*self.weight.unsqueeze(1)
+        bn_logdet = (torch.log(mul).sum()*self.height*self.width).expand(self.batchsize)
+        #print(f"BN logdet: {bn_logdet}")
+        return bn_logdet
+
     def inverse(self,y):
         assert not self.training, "inverse must be computed in eval mode"
         batchsize, channels, height, width = y.size()

invertible/downsample.py
@@ -77,8 +77,9 @@ class keepChannels(nn.Module):
         return x_new,z
     def inverse(self,output):
         x_small,z_large = output
-        z_extra = z_large.pop(-1)
-        x = merge(x_small,z_extra)
+        #z_extra = z_large.pop(-1)
+        x,z_large = merge(x_small,z_large[-1]),z_large[:-1]
+        #x = merge(x_small,z_extra)
         return x, z_large
     def logdet(self):
         return 0

invertible/spectral_norm.py
@@ -28,10 +28,11 @@ class SN(nn.Module):
         if self._u is None:
             random_vec = torch.randn_like(x)[:1]
             self._u = random_vec/random_vec.norm()
-        u_and_mbx = torch.cat([self._u, x], 0)
-        v_and_mby = self.module(u_and_mbx)
-        v_, mby = torch.split(v_and_mby,[1,bs])
-        v = v_.detach()
-        self._s = (self._u*v).sum()
-        self._u = v/v.norm()
+        zeros = torch.zeros_like(self._u)
+        zeros_u_and_mbx = torch.cat([zeros,self._u, x], 0)
+        zeros_v_and_mby = self.module(zeros_u_and_mbx)
+        bias,v_, mby = torch.split(zeros_v_and_mby,[1,1,bs])
+        v = v_.detach()-bias
+        self._s = torch.abs((self._u*v).sum())
+        self._u = (v/v.norm()).detach()
         return mby/torch.max(self._s,torch.tensor(1.).to(x.device))