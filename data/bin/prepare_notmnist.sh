7e7894116bce35ec52d7595146309f067b2dafd6
Pavel Izmailov <pi49@graphite.coecis.cornell.edu>
2019-04-23 16:27:40 -0400

    added data preparation script for notMNIST

---
+++ data/bin/prepare_notmnist.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "Downloading and unpacking NotMNIST"
+IMGDIR="$DIR/../images/notmnist"
+mkdir -p $IMGDIR
+curl -o $IMGDIR/notMNIST_small.tar.gz http://yaroslavvb.com/upload/notMNIST/notMNIST_small.tar.gz
+gunzip $IMGDIR/notMNIST_small.tar.gz
+tar -xf $IMGDIR/notMNIST_small.tar
