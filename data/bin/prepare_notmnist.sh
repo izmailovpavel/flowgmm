2c3dfbb5fce7d9748b3160ca9e86c9ba32e63c2a
Pavel Izmailov <izmailovpavel@gmail.com>
2019-04-24 13:34:46 -0400

    added file renaming to the notmnist script

--- data/bin/prepare_notmnist.sh
+++ data/bin/prepare_notmnist.sh
@@ -6,3 +6,12 @@ mkdir -p $IMGDIR
 curl -o $IMGDIR/notMNIST_small.tar.gz http://yaroslavvb.com/upload/notMNIST/notMNIST_small.tar.gz
 gunzip $IMGDIR/notMNIST_small.tar.gz
 tar -xf $IMGDIR/notMNIST_small.tar -C $IMGDIR
+for labeldir in $IMGDIR/notMNIST_small/*
+do
+    i=0
+    for img in $labeldir/*
+    do
+        ((i+=1))
+        mv $img ${labeldir}/${i}.png
+    done
+done
